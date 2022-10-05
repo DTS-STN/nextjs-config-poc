01cb33423d758f4fee1c59c85fbb7600e62bfe0f
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2022-10-05 19:14:36 -0230

    Add ClientConfig type

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 
+const serverRuntimeConfig = Object.keys(process.env)
+  .filter(key => key.startsWith('NEXT_PUBLIC'))
+  .reduce((previousValue, currentValue) => ({ ...previousValue, [currentValue]: process.env[currentValue] }), {})
+
 const nextConfig = {
   reactStrictMode: true,
-  serverRuntimeConfig: Object.keys(process.env)
-    .filter(key => key.startsWith('NEXT_PUBLIC'))
-    .reduce((obj, key) => ({ ...obj, [key]: process.env[key] }), {}),
+  serverRuntimeConfig,
   swcMinify: true,
 }
 
--- src/pages/api/client-config.ts
+++ src/pages/api/client-config.ts
@@ -1,10 +1,15 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import getConfig from "next/config"
+import getConfig from 'next/config'
+
+type ClientConfig = {
+  [key: string]: string | undefined
+}
 
 const { serverRuntimeConfig } = getConfig()
 
-const clientConfig = (req: NextApiRequest, res: NextApiResponse<{ name: string }>) => {
+const clientConfig = (req: NextApiRequest, res: NextApiResponse<ClientConfig>) => {
   res.status(200).json(serverRuntimeConfig)
 }
 
 export default clientConfig
+export type { ClientConfig }
