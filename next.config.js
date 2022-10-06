8605f918baa7ccddfdf7bbd0d7d6ba5cdf9fc8e3
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2022-10-06 08:27:21 -0230

    Move variable filtering to API endpoint

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const serverRuntimeConfig = Object.keys(process.env)
-  .filter(key => key.startsWith('NEXT_PUBLIC'))
-  .reduce((previousValue, currentValue) => ({ ...previousValue, [currentValue]: process.env[currentValue] }), {})
-
 const nextConfig = {
   reactStrictMode: true,
-  serverRuntimeConfig,
   swcMinify: true,
 }
 
--- src/pages/api/client-config.ts
+++ src/pages/api/client-config.ts
@@ -1,14 +1,15 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import getConfig from 'next/config'
 
 type ClientConfig = {
   [key: string]: string | undefined
 }
 
-const { serverRuntimeConfig } = getConfig()
+const nextPublicConfigs = Object.keys(process.env)
+  .filter(key => key.startsWith('NEXT_PUBLIC'))
+  .reduce((previousValue, currentValue) => ({ ...previousValue, [currentValue]: process.env[currentValue] }), {})
 
 const clientConfig = (req: NextApiRequest, res: NextApiResponse<ClientConfig>) => {
-  res.status(200).json(serverRuntimeConfig)
+  res.status(200).json(nextPublicConfigs)
 }
 
 export default clientConfig
